9e1e11ca88bf6294ebb3b250c6ed8157165114f0
mahmoudshal84 <mahmoudshal84@users.noreply.github.com>
2025-04-03 12:39:28 +0000

    deploy: e44788cc70f670c789f9c0f98e74036c0380bd6d

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
-// Firebase Configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyCP51P7jBtgK425eugqv7UwW3stAA2kaYE",
-    authDomain: "ym-lex-nn-website.firebaseapp.com",
-    projectId: "ym-lex-nn-website",
-    storageBucket: "ym-lex-nn-website.firebasestorage.app",
-    messagingSenderId: "359813736996",
-    appId: "1:359813736996:web:e745bdb4cf8356183486b1",
-    measurementId: "G-TM5HCSDV8L"
+  apiKey: "AIzaSyCP51P7jBtgK425eugqv7UwW3stAA2kaYE",
+  authDomain: "ym-lex-nn-website.firebaseapp.com",
+  projectId: "ym-lex-nn-website",
+  storageBucket: "ym-lex-nn-website.firebasestorage.app",
+  messagingSenderId: "359813736996",
+  appId: "1:359813736996:web:e745bdb4cf8356183486b1",
+  measurementId: "G-TM5HCSDV8L"
 };
 
 // Initialize Firebase
