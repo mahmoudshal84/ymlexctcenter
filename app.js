e20b12eceb17207ca6dd97912e55bf0162e21223
mahmoudshal84 <mahmoudshal84@gmail.com>
2025-04-03 08:34:56 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
-// Firebase Configuration - Injected during build process
-// DO NOT MODIFY THIS COMMENT - IT'S USED AS A MARKER FOR INJECTION
+// Firebase Configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCP51P7jBtgK425eugqv7UwW3stAA2kaYE",
+    authDomain: "ym-lex-nn-website.firebaseapp.com",
+    projectId: "ym-lex-nn-website",
+    storageBucket: "ym-lex-nn-website.firebasestorage.app",
+    messagingSenderId: "359813736996",
+    appId: "1:359813736996:web:e745bdb4cf8356183486b1",
+    measurementId: "G-TM5HCSDV8L"
+};
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
